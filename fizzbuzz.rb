ed3a237f2fb7822a0cfb14697763500a4a9c398d
Andy Maskiell <maskiella@gmail.com>
2017-10-24 21:15:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,12 @@
-  def fizzbuzz
-
+  def fizzbuzz(int)
+    if int % 15 == 0
+      puts "FizzBuzz"
+    elseif int % 3 == 0
+    puts "Fizz"
+  elseif int % 5 == 0
+  puts "Buzz"
+else
+  puts "nil"
+    end
   end
   
